4ca0ee9a58f96dfb7acb6a92c225c677d409a346
Kruti Patel <krutimevada3008@gmail.com>
2017-06-29 22:33:58 +1000

    - Setting up contact us, about us, team etc routes

--- components/app-navigation.vue
+++ components/app-navigation.vue
@@ -2,9 +2,10 @@
 nav.pa3.pa4-ns.ttu
   a.link.dim.black.b.f6.f5-ns.dib.mr3(href='#' title='Home') Young Coders
   a.link.dim.gray.f6.f5-ns.dib.mr3(href='#' title='Home') Home
-  a.link.dim.gray.f6.f5-ns.dib.mr3(href='#' title='About') About
-  a.link.dim.gray.f6.f5-ns.dib.mr3(href='#' title='Store') Blog
-  a.link.dim.gray.f6.f5-ns.dib(href='#' title='Contact') Contact
+  a.link.dim.gray.f6.f5-ns.dib.mr3(href='store' title='Store') Blog
+  a.link.dim.gray.f6.f5-ns.dib(href='team' title='The Team') The Team
+  a.link.dim.gray.f6.f5-ns.dib(href='contactus' title='Contact') Contact
+  a.link.dim.gray.f6.f5-ns.dib.mr3(href='aboutus' title='About') About
 </template>
 
 <script>
