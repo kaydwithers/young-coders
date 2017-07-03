f9d43420443a30f6deb0b354146bb475df6b806a
Kruti Patel <krutimevada3008@gmail.com>
2017-07-03 22:20:59 +1000

    - contact us form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,11 +1,51 @@
 <template lang="pug">
 div
   app-hero(:hero="hero" heroSmall)
-  .bg-white
-    .mw9.center.ph3.ph5-ns.pv6
-      .w-100
+  head
+    style.
+      input[type=text], select, textarea {
+      width: 100%;
+      padding: 12px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+      margin-top: 6px;
+      margin-bottom: 16px;
+      resize: vertical;
+      }
+      input[type=submit] {
+      background-color: #4CAF50;
+      color: white;
+      padding: 12px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      }
+      input[type=submit]:hover {
+      background-color: #45a049;
+      }
+      .container {
+      border-radius: 5px;
+      background-color: #f2f2f2;
+      padding: 20px;
+      }
+  body
+    .bg-white
+      .mw9.center.ph3.ph5-ns.pv6
+        .w-100
         app-heading(size='2' :text='page.fields.heading')
         app-paragraph(size='4' :text='page.fields.content')
+        .container
+            form(action='/action_page.php')
+              label(for='fname') First Name
+              input#fname(type='text', name='firstname', placeholder='Your name..')
+              label(for='lname') Last Name
+              input#lname(type='text', name='lastname', placeholder='Your last name..')
+              label(for='email') Email*
+              input#email(type='text', name='email', placeholder='Your Email Id..')
+              label(for='subject') Subject
+              textarea#subject(name='subject', placeholder='Write something..', style='height:200px')
+              input(type='submit', value='Submit')
 </template>
 
 <script>
@@ -39,6 +79,7 @@ export default {
     appParagraph
   }
 }
+  
 </script>
 
 <style>
