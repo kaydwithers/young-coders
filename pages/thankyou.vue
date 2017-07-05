93ff00d17fb054db62e807a4d845ca3955bfaf82
Kruti Patel <krutimevada3008@gmail.com>
2017-07-05 23:56:19 +1000

    - contact us thank you form

---
+++ pages/thankyou.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+div
+  app-hero(:hero='hero' text='Contact us' heroSmall)
+  .bg-white
+    .mw9.center.ph3.ph4-ns.pv6
+      .w-100
+         form.black-80(name='thanks')
+          .measure
+            .mb4
+              label.f6.b.db.mb2(for='fname') Thank you for contacting us. We will get back to you soon.
+  </div>
+</form>
+</template>
+
+<script>
+import { cdaClient } from '~/plugins/contentful-client.js'
+import appHeading from '~/components/atoms/app-heading'
+import appHero from '~/components/organisms/app-hero'
+import appParagraph from '~/components/atoms/app-paragraph'
+
+export default {
+  name: 'about',
+  asyncData ({ params }) {
+    return Promise.all([
+      cdaClient.getEntries({
+        'content_type': process.env.CTF_PAGE_ID,
+        order: '-sys.createdAt'
+      }),
+      cdaClient.getEntries({
+        'content_type': process.env.CTF_HERO_ID,
+        order: '-sys.createdAt'
+      })
+    ]).then(([page, hero]) => {
+      return {
+        hero: hero.items[0],
+        page: page.items[2]
+      }
+    }).catch(console.error)
+  },
+  components: {
+    appHeading,
+    appHero,
+    appParagraph
+  }
+}
+</script>
+
+<style>
+
+</style>
