58d57aad9fc9d0226bf9d148a227ee63a7bbafc8
Kruti Patel <krutimevada3008@gmail.com>
2017-07-06 22:43:51 +1000

    - added illustrations

--- components/organisms/app-process.vue
+++ components/organisms/app-process.vue
@@ -2,20 +2,20 @@
 .bg-lg-green
   .mw9.center.ph3.ph4-ns.pv5.pv6-ns
     .w-100.tc
-      app-heading(size='2' text='How it works' customClass='white')
-
+      app-heading(size='2', text='How it works', customclass='white')
     .db.flex-ns.tc-ns
       .w-100.w-25-ns.ph4-ns
-        app-paragraph(size='4' text='1. Join us at our location.' customClass='white')
-
+        img(src='//images.contentful.com/f25yij3y7fx1/2pbTltxs32EyCicKa4864I/315fda947c3e4463273cf2d16b79bcf3/step_1.png', alt='Step1', height='42', width='42')
+        app-paragraph(size='4', text='1. Join us at our location.', customclass='white')
       .w-100.w-25-ns.ph4-ns
-        app-paragraph(size='4' text='2. Learn some code. Build animations, get help from professionals.' customClass='white')
-
+        img(src='//images.contentful.com/f25yij3y7fx1/2pbTltxs32EyCicKa4864I/315fda947c3e4463273cf2d16b79bcf3/step_2.png', alt='Step2', height='42', width='42')
+        app-paragraph(size='4', text='2. Learn some code. Build animations, get help from professionals.', customclass='white')
       .w-100.w-25-ns.ph4-ns
-        app-paragraph(size='4' text='3. Show off things you have built to your friends.' customClass='white')
-
+        img(src='//images.contentful.com/f25yij3y7fx1/2pbTltxs32EyCicKa4864I/315fda947c3e4463273cf2d16b79bcf3/step_3.png', alt='Step3', height='42', width='42')
+        app-paragraph(size='4', text='3. Show off things you have built to your friends.', customclass='white')
       .w-100.w-25-ns.ph4-ns
-        app-paragraph(size='4' text='4. Return once a month to learn new skills.' customClass='white')
+        img(src='//images.contentful.com/f25yij3y7fx1/2pbTltxs32EyCicKa4864I/315fda947c3e4463273cf2d16b79bcf3/step_4.png', alt='Step4', height='42', width='42')
+        app-paragraph(size='4', text='4. Return once a month to learn new skills.', customclass='white')
 </template>
 
 <script>
