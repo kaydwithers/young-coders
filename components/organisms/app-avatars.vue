683a98ce6754b48c8b64aa3b38c18ac6d1ee54b3
Kruti Patel <krutimevada3008@gmail.com>
2017-07-26 23:25:44 +1000

    - avatars updated - event added

--- components/organisms/app-avatars.vue
+++ components/organisms/app-avatars.vue
@@ -6,43 +6,58 @@ div
     .w-100.w-25-ns.ph4.mb4.tc
       img.db.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499400475/Young%20Coders/kruti.jpg')
       app-paragraph(size='5', text='Kruti' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Founder' custom-class='white-70 mb0')
+      //app-paragraph(size='6', text='Founder' custom-class='white-70 mb0')
+
+    .w-100.w-25-ns.ph4.mb4.tc
+      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvkozhom2/image/upload/v1501072666/kayd_qzu2mg.jpg')
+      app-paragraph(size='5', text='Kayd Withers' custom-class='white mb0 lh-1')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
       img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499400476/Young%20Coders/kavi.jpg')
       app-paragraph(size='5', text='Kavita Mevada' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Lead Workshop Manager' custom-class='white-70 mb0')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
       img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499400475/Young%20Coders/hayley.jpg')
       app-paragraph(size='5', text='Hayley Carra' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Editor-in-Chief' custom-class='white-70 mb0')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
       img.db.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499400475/Young%20Coders/shu.jpg')
       app-paragraph(size='5', text='Shuyen Phoon' custom-class='white mb0 lh-1') 
-      app-paragraph(size='6', text='Editor-in-Chief' custom-class='white-70 mb0')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
       img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499400476/Young%20Coders/sharath.jpg')
       app-paragraph(size='5', text='Sharath Raj' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Web Developer' custom-class='white-70 mb0')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
       img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499400476/Young%20Coders/dolly.jpg')
       app-paragraph(size='5', text='Dolly Desai' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Event Organizer' custom-class='white-70 mb0')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
+    
+    .w-100.w-25-ns.ph4.mb4.tc
+      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvkozhom2/image/upload/v1501073228/michael_j4qdnp.jpg')
+      app-paragraph(size='5', text='Michael Riley' custom-class='white mb0 lh-1')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
-      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499262182/avatar-4_ehwbbi.jpg')
-      app-paragraph(size='5', text='Raffi Martins' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Art Director' custom-class='white-70 mb0')
+      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvkozhom2/image/upload/v1501072665/natasja_hwni49.jpg')
+      app-paragraph(size='5', text='Natasja Laurie' custom-class='white mb0 lh-1')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
 
     .w-100.w-25-ns.ph4.mb4.tc
-      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvjpaoffl/image/upload/q_auto,q_auto,w_auto,f_auto/v1499262182/avatar-1_swz7b2.jpg')
-      app-paragraph(size='5', text='Kayd Withers' custom-class='white mb0 lh-1')
-      app-paragraph(size='6', text='Web Developer' custom-class='white-70 mb0')
-  
+      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvkozhom2/image/upload/v1501073020/abi_kgnxtj.jpg')
+      app-paragraph(size='5', text='Abigail Corpus' custom-class='white mb0 lh-1')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
+ 
+    .w-100.w-25-ns.ph4.mb4.tc
+      img.ba.br-100.bw3.b--black-10.mb4(src='http://res.cloudinary.com/dvkozhom2/image/upload/v1501073574/abby_kv1pge.jpg')
+      app-paragraph(size='5', text='Abby Goode' custom-class='white mb0 lh-1')
+      //app-paragraph(size='6', text='Volunteer' custom-class='white-70 mb0')
+ 
   //- .tc
   //- app-heading(size='2' text='Board Of Directors')
   //- .db.flex-ns
--- components/organisms/app-events.vue
+++ components/organisms/app-events.vue
@@ -2,19 +2,25 @@
 .bg-white
   .mw9.center.ph3.ph4-ns.pv5.pv6-ns.tc
    .db.flex-ns.justify-center.flex-wrap
-    .w-100.w-25-ns.ph4.mb4.tc
+    .w-100.w-50-ns.ph4.mb4.tc
       app-heading(size='2', text='Upcoming Events')
       .db.flex-ns
         .w-100.ph4
-          app-paragraph(size='4', text='Please watch this space for Upcoming Events or Email us at hello@youngcodersau.com' custom-class='black-50 mb0') 
-    .w-100.w-25-ns.ph4.mb4.tc
+          app-paragraph(size='4', text='Coding 102: Website with HTML & CSS' custom-class='black-50 mb0')
+          app-paragraph(size='4', text='On 12 August 2017' custom-class='black-50 mb0')
+          app-paragraph(size='4', text='Place: Isobar Australia, Level 4, 111 Cecil Street, South Melbourne' custom-class='black-50 mb4')
+          app-paragraph(size='4', text='Time : 12:00 pm to 2:00 pm' custom-class='black-50 mb4') 
+          a(href='https://www.eventbrite.com.au/e/coding-102-website-with-html-css-tickets-36465643722?ref=ebtn', target='_blank')
+            img(src='https://www.eventbrite.com.au/custombutton?eid=36465643722', alt='Eventbrite - Coding 102: Website with HTML & CSS')
+
+    .w-100.w-50-ns.ph4.mb4.tc
       app-heading(size='2', text='Past Events')
       .db.flex-ns
         .w-100.ph4
           app-paragraph(size='4', text='Coding 101 : Building a Static Website' custom-class='black-50 mb0')
           app-paragraph(size='4', text='On 22 July 2017' custom-class='black-50 mb0')
           app-paragraph(size='4', text='Place:  25 Glenferrie road Malvern' custom-class='black-50 mb4')
-          app-paragraph(size='4', text='Time : 12 pm to 2:00 pm' custom-class='black-50 mb4') 
+          app-paragraph(size='4', text='Time : 12:00 pm to 2:00 pm' custom-class='black-50 mb4') 
 </template>
 
 <script>
