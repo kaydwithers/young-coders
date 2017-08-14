81046aa8f6d912d602690199887524bb08c79956
kruti.patel <kruti.patel@isobar.com>
2017-08-14 15:51:38 +1000

    - Google Analytics

--- plugins/ga.js
+++ plugins/ga.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
   /*
   ** Set the current page
   */
-  ga('create', 'UA-XXXXXXXX-X', 'auto')
+  ga('create', 'UA-104501170-1', 'auto')
   /*
   ** Every time the route changes (fired on initialization too)
   */
