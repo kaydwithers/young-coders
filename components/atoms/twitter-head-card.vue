9dba1bab5fd3174235edd82fa037d20de243ccd7
kaydwithers <kaydwithers@github.com>
2017-08-20 21:31:19 +1000

    add share image

--- components/atoms/twitter-head-card.vue
+++ components/atoms/twitter-head-card.vue
@@ -14,7 +14,7 @@ export default {
       { name: 'twitter:site', content: 'youngcodersau.com' },
       { name: 'twitter:title', content: 'Young Coders ʕ•ᴥ•ʔ' },
       { name: 'twitter:description', content: 'Coding workshops for kids and all ages' },
-      { name: 'twitter:image', content: '/assets/young-coders-share' },
+      { name: 'twitter:image', content: 'https://res.cloudinary.com/dvjpaoffl/image/upload/v1503228580/young-coders-share.jpg' },
       { name: 'twitter:image:alt', content: 'Young Coders logo' }
     ]
   }
